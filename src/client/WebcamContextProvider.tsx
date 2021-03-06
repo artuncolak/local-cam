8d53b6e623df95d542c8e569cbd6970d990e785a
Artun Çolak <artuncolak97@gmail.com>
2021-03-06 22:22:25 +0300

    URL removed from the context

--- src/client/WebcamContextProvider.tsx
+++ src/client/WebcamContextProvider.tsx
@@ -3,21 +3,16 @@ import { createContext, Dispatch, SetStateAction, useState } from "react";
 interface ContextProps {
   id: string;
   setId: Dispatch<SetStateAction<string>>;
-  url: string;
-  setUrl: Dispatch<SetStateAction<string>>;
 }
 
 export const WebcamContext = createContext<Partial<ContextProps>>({});
 
 export default function WebcamContextProvider({ children }) {
   const [id, setId] = useState<string>();
-  const [url, setUrl] = useState<string>();
 
   const values = {
     id,
     setId,
-    url,
-    setUrl,
   };
 
   return (
